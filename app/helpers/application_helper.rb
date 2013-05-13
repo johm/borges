8ad08c72053e188d2f9ec728277764353c8544e9
John Duda <john@sylvie.(none)>
2013-05-13 08:29:10 -0400

    make drier bootstrap forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  
+  def mytextfield(f,l,m)
+    content_tag(:div,
+                f.label(l,:class=>"control-label") +
+                content_tag(:div,
+                            f.text_field(m),
+                            :class=>"controls"),
+                :class=>"field control-group")
+  end
+
+  def mytextarea(f,l,m)
+    content_tag(:div,
+                f.label(l,:class=>"control-label") +
+                content_tag(:div,
+                            f.text_field(m),
+                            :class=>"controls"),
+                :class=>"field control-group")
+  end
+
+
+  def mysubmit(f,l)
+    content_tag(:div,
+                content_tag(:div,
+                            f.submit(:value=>l,:class=>"btn btn-large btn-primary"),
+                            :class=>"controls"
+                            ),
+                :class=>"actions control-group")
+  end
+
 end
