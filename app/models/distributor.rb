530c72ebeab04bfa57a421a6707fd422cc6194a4
John Duda <john@sylvie.(none)>
2013-07-06 16:56:50 -0400

    put some stuff in the wrong place!

--- app/models/distributor.rb
+++ app/models/distributor.rb
@@ -1,6 +1,10 @@
 class Distributor < ActiveRecord::Base
   attr_accessible :name, :notes, :our_account_number
 
+  has_many :copies
+  has_many :editions, :through => :copies
+
+
   def to_s 
     name_and_id
   end
