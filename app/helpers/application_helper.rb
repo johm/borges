20abacd1dca68933be7c6f4a1e639dd5e239b875
Robin Hoode <robinhoode@gmail.com>
2013-10-20 11:46:42 -0400

    Refactor theme partial into it's own method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,130 +1,132 @@
 module ApplicationHelper
+  def theme_partial(partial_name)
+    render partial: "themes/#{theme}/#{partial_name}"
+  end
 
-  
   def mytextfield(f,l,m,options={})
     content_tag(:div,
-                f.label(l,:class=>"control-label") +
-                content_tag(:div,
-                            f.text_field(m,options),
-                            :class=>"controls"),
-                :class=>"field control-group")
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        f.text_field(m,options),
+        :class=>"controls"),
+      :class=>"field control-group")
   end
 
   def mymoneyfield(f,l,m,options={})
     content_tag(:div,
-                f.label(l,:class=>"control-label") +
-                content_tag(:div,
-                            content_tag(:span,"$",:class=>"add-on")+f.text_field(m,options),
-                            :class=>"controls  input-prepend",:style=>"margin-left:20px"),
-                :class=>"field control-group")
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        content_tag(:span,"$",:class=>"add-on")+f.text_field(m,options),
+        :class=>"controls  input-prepend",:style=>"margin-left:20px"),
+      :class=>"field control-group")
   end
 
   def mypercentfield(f,l,m,options={})
     content_tag(:div,
-                f.label(l,:class=>"control-label") +
-                content_tag(:div,
-                            f.text_field(m,options)+content_tag(:span,"%",:class=>"add-on"),
-                            :class=>"controls  input-append",:style=>"margin-left:20px"),
-                :class=>"field control-group")
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        f.text_field(m,options)+content_tag(:span,"%",:class=>"add-on"),
+        :class=>"controls  input-append",:style=>"margin-left:20px"),
+      :class=>"field control-group")
   end
 
 
   def myselector(f,l,m,ofs,options={})
     content_tag(:div,
-                f.label(l,:class=>"control-label") +
-                content_tag(:div,
-                            f.select(m,ofs,options),
-                            :class=>"controls"),
-                :class=>"field control-group")
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        f.select(m,ofs,options),
+        :class=>"controls"),
+      :class=>"field control-group")
   end
 
 
 
- def mycheckbox(f,l,m,options={})
-   content_tag(:div,
-               f.label(l,:class=>"control-label") +
-               content_tag(:div,
-                           content_tag(:div,
-                                       f.check_box(m),
-                                       options.merge({:class=>"switch switch-large"})),
-                           :class=>"controls"),
-               :class=>"field control-group")
- end
+  def mycheckbox(f,l,m,options={})
+    content_tag(:div,
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        content_tag(:div,
+          f.check_box(m),
+          options.merge({:class=>"switch switch-large"})),
+        :class=>"controls"),
+      :class=>"field control-group")
+  end
 
   def mytextarea(f,l,m,options={})
     content_tag(:div,
-                f.label(l,:class=>"control-label") +
-                content_tag(:div,
-                            f.cktext_area(m,options.merge({:class=>"input-xxlarge"})),
-                            :class=>"controls"),
-                :class=>"field control-group")
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        f.cktext_area(m,options.merge({:class=>"input-xxlarge"})),
+        :class=>"controls"),
+      :class=>"field control-group")
   end
 
 
   def myplaintextarea(f,l,m,options={})
     content_tag(:div,
-                f.label(l,:class=>"control-label") +
-                content_tag(:div,
-                            f.text_area(m,options.merge({:class=>"input-xxlarge"})),
-                            :class=>"controls"),
-                :class=>"field control-group")
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        f.text_area(m,options.merge({:class=>"input-xxlarge"})),
+        :class=>"controls"),
+      :class=>"field control-group")
   end
 
 
   def mysubmit(f,l,options={},size="btn-large")
     content_tag(:div,
-                content_tag(:div,
-                            f.submit(options.merge({:value=>l,:class=>"btn btn-primary #{size}"})),
-                            :class=>"controls"
-                            ),
-                :class=>"actions control-group")
+      content_tag(:div,
+        f.submit(options.merge({:value=>l,:class=>"btn btn-primary #{size}"})),
+        :class=>"controls"
+      ),
+      :class=>"actions control-group")
   end
 
- 
+
   def mylinkbutton(text,path,options={},size="btn-large")
     content_tag(:div,
-                content_tag(:a,
-                            raw(text),
-                            options.merge({:href=>path,:class=>"btn #{size} btn-primary controls"})
-                            ),
-                :class=>"actions control-group")
+      content_tag(:a,
+        raw(text),
+        options.merge({:href=>path,:class=>"btn #{size} btn-primary controls"})
+      ),
+      :class=>"actions control-group")
   end
 
 
   def mypostlinkbutton(text,path,url_options={},html_options={},size="btn-large")
     content_tag(:div,
-                link_to(raw(text),
-                        path,
-                        url_options.merge({:method=>:post,:class=>"btn #{size} btn-primary controls"})
-                        ),
-                :class=>"actions control-group")
+      link_to(raw(text),
+        path,
+        url_options.merge({:method=>:post,:class=>"btn #{size} btn-primary controls"})
+      ),
+      :class=>"actions control-group")
   end
 
 
- 
+
   def myselectfield (f,m,l,p,a=false)
     my_unique_id=SecureRandom.uuid
     content_tag(:div,
-                f.label(l,:class=>"control-label") +
-                content_tag(:div,
-                            f.autocomplete_field(m,p,:id_element=>"##{m}-#{my_unique_id}",:class=>"theautocomplete")+ 
-                            f.hidden_field("#{m}_id",:id=>"#{m}-#{my_unique_id}",:class=>"theid") + 
-                            " " +
-                            (a ? content_tag(:a,
-                                                  "+",
-                                        {:href=>a,:class=>"btn",:target=>"_blank"}
-                                        ) : ""),
-                            :class=>"controls"),
-                :class=>"field control-group")
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        f.autocomplete_field(m,p,:id_element=>"##{m}-#{my_unique_id}",:class=>"theautocomplete")+
+        f.hidden_field("#{m}_id",:id=>"#{m}-#{my_unique_id}",:class=>"theid") +
+        " " +
+        (a ? content_tag(:a,
+            "+",
+            {:href=>a,:class=>"btn",:target=>"_blank"}
+          ) : ""),
+        :class=>"controls"),
+      :class=>"field control-group")
   end
 
   def my_autocomplete_field_tag (f,m,l,p,extra="")
     content_tag(:div,
-                f.label(l,:class=>"control-label") +
-                content_tag(:div,
-                            autocomplete_field_tag(m,"",p) + " " + extra,
-                            :class=>"controls"),
-                :class=>"field control-group")
+      f.label(l,:class=>"control-label") +
+      content_tag(:div,
+        autocomplete_field_tag(m,"",p) + " " + extra,
+        :class=>"controls"),
+      :class=>"field control-group")
   end
 
 
@@ -138,15 +140,12 @@ module ApplicationHelper
   def theme
     ENV["THEME"] || "default"
   end
-    
 
   def sitename
-      ENV["SITENAME"] || "Borges"
+    ENV["SITENAME"] || "Borges"
   end
 
   def layouts
-      YAML.load(ENV["LAYOUTS"]) || ["application"]
+    YAML.load(ENV["LAYOUTS"]) || ["application"]
   end
-
-  
 end
