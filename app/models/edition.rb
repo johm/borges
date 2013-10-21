b4191f578d0e6ed2dbc6d83f0183dbca0b775304
Robin Hoode <robinhoode@gmail.com>
2013-10-20 20:44:23 -0400

    Use default_value_for to DRY up the Edition model

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -2,10 +2,10 @@ class Edition < ActiveRecord::Base
   belongs_to :title
   has_many :copies
   has_many :purchase_order_line_items
-  has_many :invoice_line_items 
-  
+  has_many :invoice_line_items
+
   has_many :distributors, :through => :copies
-  
+
   belongs_to :publisher
 
   attr_accessible :format, :in_print, :isbn10, :isbn13, :notes, :year_of_publication, :list_price, :cover ,:publisher_id,:remote_cover_url,:publisher,:title_id
@@ -16,19 +16,24 @@ class Edition < ActiveRecord::Base
   mount_uploader :cover, ImageUploader
 
   monetize :list_price_cents
+  default_value_for :list_price_cents, 0
 
   scope :published, where(:in_print => true)
+  default_value_for :in_print, true
+
   scope :newest_first, order("year_of_publication desc")
-  scope :without_edition, lambda{|e| e ? {:conditions => ["id != ?", e.id]} : {} }
+  scope :without_edition, lambda {|e| e ? {:conditions => ["id != ?", e.id]} : {} }
+
+  default_value_for :format, "Paperback"
 
   searchable do
     text :isbn10,:isbn13
-    
-    text :title do 
+
+    text :title do
       title.title
     end
-  end  
-  
+  end
+
   def has_copies_in_stock?
     copies.where("status"=>"STOCK").length > 0
   end
@@ -40,7 +45,6 @@ class Edition < ActiveRecord::Base
   def my_online_price
     has_copies_in_stock? ? copies.where("status"=>"STOCK").order("price_in_cents desc").first.price : ""
   end
-  
 
   def self.formats
     ['Hardcover','Paperback','Pamphlet','Magazine','Journal','CD','DVD','Clothing','Other']
@@ -54,13 +58,13 @@ class Edition < ActiveRecord::Base
     isbn13
   end
 
-  def isbns_are_valid 
+  def isbns_are_valid
     unless isbn10.blank? || Lisbn.new(isbn10).valid?
-        errors.add(:isbn10,"Not valid")
+      errors.add(:isbn10,"Not valid")
     end
-    
+
     unless isbn13.blank? || Lisbn.new(isbn13).valid?
-        errors.add(:isbn13,"Not valid")
+      errors.add(:isbn13,"Not valid")
     end
   end
 
@@ -72,14 +76,10 @@ class Edition < ActiveRecord::Base
       self.isbn13=Lisbn.new(isbn10).isbn13
     end
   end
-  
+
   protected
   def normalize_isbns
     isbn10.gsub!(/[^0-9X]/,'') unless isbn10.nil?
     isbn13.gsub!(/[^0-9X]/,'') unless isbn13.nil?
   end
-
-
-
-
 end
