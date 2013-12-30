805ef6ace2d5dca021950624765c817aab3608c5
John Duda <john@redemmas.org>
2013-12-30 09:30:03 -0500

    cachestagram

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,24 +11,26 @@ class HomeController < ApplicationController
 
   
   def frontpage
-    Instagram.configure do |config|
-      config.client_id =     ENV["INSTAGRAM_ID"]
-      config.access_token =  ENV["INSTAGRAM_SECRET"]
-    end    
 
-    client = Instagram.client()
-    user = client.user
+    @instagram = Rails.cache.fetch("instafront",:expires_in => 10.minutes) do
+      Instagram.configure do |config|
+        config.client_id =     ENV["INSTAGRAM_ID"]
+        config.access_token =  ENV["INSTAGRAM_SECRET"]
+      end    
+      
+      client = Instagram.client()
+      user = client.user
     
-    html = "<ul class='thumbnails frontstagram'>"
-    i=0
-    for media_item in client.user_recent_media()
-      break if i>2
-      i=i+1
-      html << "<li class='span2'><div class='thumbnail'><div><a target='_blank' title='#{media_item.caption.text.gsub(/'/,'&quot;')}' href='#{media_item.link}'><img width='100%' src='#{media_item.images.low_resolution.url}'></div></a></div></li>"
+      html = "<ul class='thumbnails frontstagram'>"
+      i=0
+      for media_item in client.user_recent_media()
+        break if i>2
+        i=i+1
+        html << "<li class='span2'><div class='thumbnail'><div><a target='_blank' title='#{media_item.caption.text.gsub(/'/,'&quot;')}' href='#{media_item.link}'><img width='100%' src='#{media_item.images.low_resolution.url}'></div></a></div></li>"
+      end
+      html << "</ul>"
+      html
     end
-    html << "</ul>"
-    @instagram=html
-
 
     render :layout => "frontpage"
   end
@@ -42,6 +44,8 @@ class HomeController < ApplicationController
   end
 
   def food
+
+    @instagram = Rails.cache.fetch("instafood",:expires_in => 10.minutes) do
     Instagram.configure do |config|
       config.client_id =     ENV["INSTAGRAM_ID"]
       config.access_token =  ENV["INSTAGRAM_SECRET"]
@@ -61,8 +65,8 @@ class HomeController < ApplicationController
       end
     end
     html << "</ul>"
-    @instagram=html
-
+    html
+  end
   end
 
 end
