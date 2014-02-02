7f63a8e42e186f039ad2a84200ef6bab8c4d0819
John Duda <john@redemmas.org>
2014-02-02 18:20:10 -0500

    validate

--- app/models/event_shift.rb
+++ app/models/event_shift.rb
@@ -2,4 +2,6 @@ class EventShift < ActiveRecord::Base
   belongs_to :event_staffer
   belongs_to :event
   attr_accessible :notes, :event_staffer_id,:event_staffer
+  validates :event_staffer, :presence => true
+
 end
