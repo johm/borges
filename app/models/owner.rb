14a90a75984d11a0ce62181f9f22ac2f17fbd801
John Duda <john@redemmas.org>
2014-04-29 22:55:54 -0400

    app/models/owner.rb

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,6 +2,9 @@ class Owner < ActiveRecord::Base
 
   attr_accessible :name, :notes
   has_many :copies
+  has_many :purchase_orders
+  has_many :invoices
+
 
   def name_and_id
     "#{name} (#{id})"
@@ -13,6 +16,30 @@ class Owner < ActiveRecord::Base
 
   def to_s
     name_and_id
+  end
+
+  def merge_stuff_from_owner(unneeded_owner_id)
+    raise "PARADOX! BAD!" if id==unneeded_owner_id
+    unneeded_owner=Owner.find(unneeded_owner_id)
+    raise "NO OWNER BY THAT ID FOUND!" if unneeded_owner.nil?
+
+    unneeded_owner.copies.each do |c|
+      c.owner = self
+      c.save!
+    end
+
+    unneeded_owner.purchase_orders.each do |p|
+      p.owner = self
+      p.save!
+    end
+
+    unneeded_owner.invoices.each do |i|
+      i.owner = self
+      i.save!
+    end
+
+    unneeded_owner.destroy
+    
   end
 
 end
