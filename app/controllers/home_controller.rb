da0233e3e1b962daa6299a70a3dda4aa64d498c9
John Duda <john@redemmas.org>
2014-05-27 17:34:54 -0400

    instagram sanity checks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,26 +12,29 @@ class HomeController < ApplicationController
   
   def frontpage
 
-    @instagram = Rails.cache.fetch("instafront",:expires_in => 10.minutes) do
-      Instagram.configure do |config|
-        config.client_id =     ENV["INSTAGRAM_ID"]
-        config.access_token =  ENV["INSTAGRAM_SECRET"]
-      end    
+    begin
+      @instagram = Rails.cache.fetch("instafront",:expires_in => 10.minutes) do
+        Instagram.configure do |config|
+          config.client_id =     ENV["INSTAGRAM_ID"]
+          config.access_token =  ENV["INSTAGRAM_SECRET"]
+        end    
       
-      client = Instagram.client()
-      user = client.user
-    
-      html = "<ul class='thumbnails frontstagram'>"
-      i=0
-      for media_item in client.user_recent_media()
-        break if i>2
-        i=i+1
-        html << "<li class='span2'><div class='thumbnail'><div><a target='_blank' title='#{media_item.caption.text.gsub(/'/,'&quot;') unless media_item.caption.nil?}' href='#{media_item.link}'><img width='100%' src='#{media_item.images.low_resolution.url.gsub(/^http:/,"")}'></div></a></div></li>"
+        client = Instagram.client()
+        user = client.user
+        
+        html = "<ul class='thumbnails frontstagram'>"
+        i=0
+        for media_item in client.user_recent_media()
+          break if i>2
+          i=i+1
+          html << "<li class='span2'><div class='thumbnail'><div><a target='_blank' title='#{media_item.caption.text.gsub(/'/,'&quot;') unless media_item.caption.nil?}' href='#{media_item.link}'><img width='100%' src='#{media_item.images.low_resolution.url.gsub(/^http:/,"")}'></div></a></div></li>"
+        end
+        html << "</ul>"
+        html
       end
-      html << "</ul>"
-      html
+    rescue
+      @instagram=""
     end
-
     render :layout => "frontpage"
   end
 
@@ -44,29 +47,33 @@ class HomeController < ApplicationController
   end
 
   def food
-
-    @instagram = Rails.cache.fetch("instafood",:expires_in => 10.minutes) do
-    Instagram.configure do |config|
-      config.client_id =     ENV["INSTAGRAM_ID"]
-      config.access_token =  ENV["INSTAGRAM_SECRET"]
-    end    
-
-    client = Instagram.client()
-    user = client.user
-    
+    begin
+      @instagram = Rails.cache.fetch("instafood",:expires_in => 10.minutes) do
+        Instagram.configure do |config|
+          config.client_id =     ENV["INSTAGRAM_ID"]
+          config.access_token =  ENV["INSTAGRAM_SECRET"]
+        end    
+        
+        client = Instagram.client()
+        user = client.user
+        
     html = "<h1>Recently delicious</h1><ul class='thumbnails'>"
-    i=0
-    for media_item in client.user_recent_media()
-      break if i>8
-      i=i+1
-      tags = media_item.tags
-      if tags.include?("food") || tags.include?("vegan")  || tags.include?("cafe") 
-        html << "<li class='span2'><div class='thumbnail'><div style='margin-bottom:10px;'><a target='_blank' href='#{media_item.link}'><img width='100%' src='#{media_item.images.low_resolution.url.gsub(/^http:/,"")}'></div>#{media_item.caption.text}</a></div></li>"
+        i=0
+        for media_item in client.user_recent_media()
+          break if i>8
+          i=i+1
+          tags = media_item.tags
+          if tags.include?("food") || tags.include?("vegan")  || tags.include?("cafe") 
+            html << "<li class='span2'><div class='thumbnail'><div style='margin-bottom:10px;'><a target='_blank' href='#{media_item.link}'><img width='100%' src='#{media_item.images.low_resolution.url.gsub(/^http:/,"")}'></div>#{media_item.caption.text}</a></div></li>"
+          end
+        end
+        html << "</ul>"
+        html
       end
+    rescue
+      @instagram=""
     end
-    html << "</ul>"
-    html
+    
   end
-  end
-
+  
 end
