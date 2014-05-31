678f66a6eb875fd5007c1beb734c2108c870d511
John Duda <john@redemmas.org>
2014-05-31 11:12:59 -0400

    where is my coffee?

--- app/helpers/titles_helper.rb
+++ app/helpers/titles_helper.rb
@@ -24,12 +24,12 @@ module TitlesHelper
 
       edition=title.latest_published_edition
       copy=edition.copies.last
-      content_tag(:div,
+      content_tag(:small,
                   "Pub: ".html_safe +
                   (edition.publisher.nil? ? "?" : link_to(edition.publisher.name, edition.publisher)).html_safe + 
-                  "Dist: ".html_safe +
+                  " Dist: ".html_safe +
                   (copy.nil? || copy.invoice_line_item.nil? ? "?" : link_to(copy.invoice_line_item.invoice.distributor,copy.invoice_line_item.invoice.distributor.try(:name))).html_safe,
-                  :class=>"small ordering_from_info"
+                  :class=>"ordering_from_info"
                   ).html_safe
 
   end
