49f032243e8b259a855df5c03d9b36acff27d292
John Duda <john@redemmas.org>
2014-07-19 18:07:56 -0400

    Add order by when field to purchase orders

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -7,7 +7,7 @@ class PurchaseOrder < ActiveRecord::Base
   belongs_to :owner #who gets the books
 
   has_many :purchase_order_line_items,dependent: :destroy
-  attr_accessible :notes, :number, :ordered, :ordered_when, :distributor_id,:owner_id
+  attr_accessible :notes, :number, :ordered, :ordered_when,:order_by_when, :distributor_id,:owner_id
 
   default_scope  includes(:purchase_order_line_items)
 
