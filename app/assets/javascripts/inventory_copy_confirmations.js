0d902e99d89ed86da135d6bf073a693290bb665c
John Duda <john@redemmas.org>
2015-01-02 17:39:29 -0500

    stop jump

--- app/assets/javascripts/inventory_copy_confirmations.js
+++ app/assets/javascripts/inventory_copy_confirmations.js
@@ -18,9 +18,9 @@ jQuery(document).ready(function() {
 
 
 
-    jQuery("#open_copies").on("click",".inventory_section_link",function(){
+    jQuery("#open_copies").on("click",".inventory_section_link",function(e){
 	jQuery.post("/inventory_copy_confirmations",{inventory_copy_confirmation: {copy_id: jQuery(this).data("copy"), inventory_id: jQuery(this).data("inventory")}},null,'script');
-	
+	e.preventDefault();
     });
 
 });
