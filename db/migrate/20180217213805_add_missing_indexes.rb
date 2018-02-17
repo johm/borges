b2c151ad955200df9f9d5b8650ecfe8d9f919a6e
John Duda <john@redemmas.org>
2018-02-17 16:42:08 -0500

    some fixes to fix

--- db/migrate/20180217213805_add_missing_indexes.rb
+++ db/migrate/20180217213805_add_missing_indexes.rb
@@ -1,7 +1,6 @@
 class AddMissingIndexes < ActiveRecord::Migration
 
   def change
-    add_index :editions, [:distributor_id, :title_id]
     add_index :shopping_carts, [:submitted,:deferred,:completed]
   end
 end
