b55bebc8b08761331d16bb6660c7622bde5531e0
John Duda <john@redemmas.org>
2018-09-18 18:00:34 -0400

    initial restyling

--- app/assets/javascripts/redemmas.js
+++ app/assets/javascripts/redemmas.js
@@ -1,7 +1,4 @@
 jQuery('document').ready(function (){
-  jQuery("#front_1").fitText(1.2);
-  jQuery("#front_2").fitText(2.4);
-  jQuery(".front_moreaboutus").fitText(3.6);
+  jQuery("#front_1 .translucent").fitText(1.2);
   jQuery(".footersocial").fitText(0.5);
-
 });
