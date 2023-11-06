208fd9ee3a89757b6eb7e25a27b549629e3b4dfd
John Duda <john@redemmas.org>
2023-11-06 19:06:44 +0000

    quantities on returns

--- app/controllers/return_order_line_items_controller.rb
+++ app/controllers/return_order_line_items_controller.rb
@@ -45,19 +45,32 @@ class ReturnOrderLineItemsController < ApplicationController
   # POST /return_order_line_items.json
   def create
     @return_order_line_item = ReturnOrderLineItem.new(params[:return_order_line_item])
-
     respond_to do |format|
-      if @return_order_line_item.save
+      @return_order_line_item.quantity = 1       unless @return_order_line_item.quantity.to_i > 1 
+      number_of_copies_left=@return_order_line_item.copy.edition.copies.instock.length
+      number_to_return=@return_order_line_item.quantity.to_i
+      if number_to_return > number_of_copies_left
+        format.js { render :js => "alert('Only #{number_of_copies_left} copies of this edition in stock');"}
+      else
+        @rolis = []
+        number_to_return.times do
+          copies=@return_order_line_item.copy.edition.copies.instock.order("price_in_cents desc")
+          copies_on_sale_orders= SaleOrder.where(:posted=>[false,nil]).collect {|so| so.sale_order_line_items.collect {|soli| soli.copy_id}}.flatten
+          copies_on_return_orders= ReturnOrder.where(:posted=>[false,nil]).collect {|ro| ro.return_order_line_items.collect {|roli| roli.copy_id}}.flatten
+          copies=copies.find_all {|c| !(copies_on_sale_orders.include? c.id)}
+          copies=copies.find_all {|c| !(copies_on_return_orders.include? c.id)}
+          
+          roli=ReturnOrderLineItem.new(:copy_id=>copies.first.id,:return_order_id=>@return_order_line_item.return_order.id)
+          roli.save!
+          @rolis << roli
+        end
         format.html { redirect_to @return_order_line_item, notice: 'Return order line item was successfully created.' }
         format.json { render json: @return_order_line_item, status: :created, location: @return_order_line_item }
-        format.js {}
-      else
-        format.html { render action: "new" }
-        format.json { render json: @return_order_line_item.errors, status: :unprocessable_entity }
         format.js {}
       end
     end
   end
+  
 
   # PUT /return_order_line_items/1
   # PUT /return_order_line_items/1.json
--- app/models/return_order_line_item.rb
+++ app/models/return_order_line_item.rb
@@ -1,14 +1,13 @@
 class ReturnOrderLineItem < ActiveRecord::Base
   belongs_to :return_order
   belongs_to :copy
-
-  attr_accessible :copy_id,:return_order_id
+  attr_accessor :quantity
+  attr_accessible :copy_id,:return_order_id,:quantity
 
 
   def do_return
     copy.do_return()
   end
-  
   
 
 end
